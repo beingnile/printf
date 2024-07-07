89c46cdf018a5df3d614b2018062edd5a393b75c
Nile Okomo <okomonile3@gmail.com>
2024-07-07 16:26:22 +0300

    Add function prototype for print_unsigned_base function

--- main.h
+++ main.h
@@ -12,6 +12,7 @@ int print_bin(va_list arg);
 int print_char(va_list arg);
 int print_int(va_list arg);
 int print_str(va_list arg);
+int print_unsigned_base(va_list arg, int base, int uppercase);
 
 /* Helper functions */
 int _itoa(int src, char *str);
